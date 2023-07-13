1c172c047f4664673deda0f79ab854f1633d0e4e
Theodore Smith <tjs7321@gmail.com>
2023-07-13 06:25:43 -0400

    finished

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const thankYou = []
+    for (let i = 0; i < names.length; i++) {
+        thankYou.push(
+            `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        )
+    }
+    return thankYou
+}
+
+function countDown(num){
+    while (num !== -1) {
+        console.log(num--)
+    }
+}
